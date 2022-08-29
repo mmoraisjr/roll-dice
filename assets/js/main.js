9d1b227e756552795e74a7010b29471e60b082ac
mmoraisjr <jr.morais.marcos@gmail.com>
2022-08-29 20:21:44 -0300

    add function sum values

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const inputDice = document.querySelector(".input__dice");
 const inputFace = document.querySelector(".input__face");
 const showDices = document.querySelector(".dices");
+const showSum = document.querySelector(".result");
 const btn = document.querySelector(".roll");
 
 btn.addEventListener("click", () => addDice());
@@ -8,21 +9,23 @@ btn.addEventListener("click", () => addDice());
 const addDice = () => {
   const dices = inputDice.value;
   const faces = inputFace.value;
-  var result;
-
+  var faceDice;
+  var sumDices = 0;
+  
   if (dices > 10) {
     console.log("o número máximo de dados é 10.");
   } else {
     for (let i = 0; i < dices; i++) {
-      result = Math.floor(Math.random() * faces) + 1;
-      console.log(result);
+      faceDice = Math.floor(Math.random() * faces) + 1;
+      console.log(faceDice);
       const newDice = document.createElement("p");
-      newDice.classList.add("square-dice")
-      newDice.textContent = result;
-      newDice.appendChild(showDices);
+      newDice.textContent = faceDice;
+      showDices.appendChild(newDice);
+      sumDices += faceDice;
+      console.log(sumDices);
     }
   }
-  const total = document.createElement("p");
-  total.classList.add("total");
-  total.textContent = result;
+  const showSumDices = document.createElement("p");
+  showSumDices.textContent = sumDices;
+  showSum.appendChild(showSumDices);
 };
