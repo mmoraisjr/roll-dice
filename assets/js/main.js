c925e943f9995e68ff11c293a9bdeec4fe672218
mmoraisjr <jr.morais.marcos@gmail.com>
2022-08-31 19:54:40 -0300

    update function random dice

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ const inputFace = document.querySelector(".input__face");
 const showDices = document.querySelector(".dices");
 const showSum = document.querySelector(".result");
 const btn = document.querySelector(".roll");
+// const faces = document.querySelectorAll(".face");
+const oldDices = JSON.parse(localStorage.getItem("oldDices")) || [];
 
 btn.addEventListener("click", () => addDice());
 
@@ -11,21 +13,32 @@ const addDice = () => {
   const faces = inputFace.value;
   var faceDice;
   var sumDices = 0;
-  
+
+  showDices.innerHTML = "";
+
   if (dices > 10) {
-    console.log("o número máximo de dados é 10.");
-  } else {
+    console.log("O número máximo de dados é 10.");
+    return
+  }if(dices == 0 || faces == 0 || dices === "" || faces === ""){
+    console.log("Preencha os campos com um valor válido");
+    return
+  }else{
     for (let i = 0; i < dices; i++) {
       faceDice = Math.floor(Math.random() * faces) + 1;
-      console.log(faceDice);
       const newDice = document.createElement("p");
+      newDice.classList.add("face");
       newDice.textContent = faceDice;
       showDices.appendChild(newDice);
       sumDices += faceDice;
-      console.log(sumDices);
     }
   }
-  const showSumDices = document.createElement("p");
-  showSumDices.textContent = sumDices;
-  showSum.appendChild(showSumDices);
+
+  oldDices.push(sumDices);
+  localStorage.setItem("oldDices", JSON.stringify(oldDices));
+  for (let i = 0; i < oldDices.length; i++) {
+    const showSumDices = document.createElement("p");
+    // console.log(oldDices[i]);
+    showSumDices.textContent = oldDices[i];
+    showSum.appendChild(showSumDices);
+  }
 };
